d6c311198ac66b372768a8920e36d0c556413735
xurongchen216169221 <53501045+xurongchen216169221@users.noreply.github.com>
2019-08-31 21:21:11 +1000

    Add files via upload

---
+++ Register.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CFSessionDB
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        private string FirstName;
+        private string LastName;
+        private int Phone;
+        private string Email;
+        private string password;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
