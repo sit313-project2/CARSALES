cb4e3c6921cf568a897ac1ae358e1572f8da7d9b
andkusuma <38446033+andkusuma@users.noreply.github.com>
2019-09-14 10:32:52 +1000

    Add files via upload

---
+++ CarsalesAppAgain/Login.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using CarsalesAppAgain.Views;
+using Xamarin.Forms;
+
+namespace CarsalesAppAgain
+{
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+            this.BackgroundImage = "wallpaper.png";
+        }
+
+        public async void SubmitCommand_Clicked(object sender, EventArgs e)
+        {
+            //await Navigation.PushAsync(new CarSalesListPage());
+
+
+            Application.Current.MainPage = new MainPage();
+
+
+            BindingContext = this;
+            await DisplayAlert("Alert", "Request Succes.", "OK");
+        }
+
+
+    }
+}
---
+++ CarsalesAppAgain/Views/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
+using Xamarin.Forms;
+using System.ComponentModel;
+
+namespace CarsalesAppAgain.Views
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : Xamarin.Forms.TabbedPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            On<Xamarin.Forms.PlatformConfiguration.Android>().SetToolbarPlacement(ToolbarPlacement.Bottom);
+        }
+    }
+}
