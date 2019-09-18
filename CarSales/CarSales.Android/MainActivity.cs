7fcebd53162389e21dbf4e11fc7014e08e6f5bad
mariam-dhanani <54351819+mariam-dhanani@users.noreply.github.com>
2019-09-18 18:09:38 +1000

    Update MainActivity.cs

--- CarSales/CarSales.Android/MainActivity.cs
+++ CarSales/CarSales.Android/MainActivity.cs
@@ -23,6 +23,11 @@ namespace CarSales.Droid
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
-        
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
     }
 }
